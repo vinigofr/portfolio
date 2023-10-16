65faeea3520904670836b9914b53c3f5ba30dd41
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2023-10-16 18:29:18 -0300

    Redirect to /me if / is inserted

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/me",
+        permanent: true,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
